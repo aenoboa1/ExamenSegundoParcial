3e8c9582118f386f34a8f0d3b56bade7d5b5c803
Andres Noboa V <aenoboa1@espe.edu.ec>
2023-07-19 16:35:37 -0500

    Initial commit

---
+++ src/main/java/ec/edu/espe/examen/ExamenSegundoParcialApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSegundoParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenSegundoParcialApplication.class, args);
+    }
+
+}
